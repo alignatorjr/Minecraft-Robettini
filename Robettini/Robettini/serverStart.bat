b614ff76c3635ddf0420e0385c668b2d9f10337d
leoni <alignatorjr@gnail.com>
2025-12-06 16:21:35 -0800

    126

--- Robettini/Robettini/serverStart.bat
+++ Robettini/Robettini/serverStart.bat
@@ -1,5 +1,7 @@
 @echo off
 REM Launch Minecraft server with Java 23
 
-"C:\Program Files\Java\jdk-23\bin\java.exe" -Xmx5024M -Xms5024M -jar server.jar
+REM "C:\Program Files\Java\jdk-23\bin\java.exe"
+
+java -Xmx5024M -Xms5024M -jar server.jar
 pause
